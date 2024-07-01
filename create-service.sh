dfc35370acebe92d9f613c56a5175b4b529e4d2d
Bill Zafeiris <bzafiris@gmail.com>
2024-07-01 12:39:41 +0300

    Fixing scripts

--- create-service.sh
+++ create-service.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
+cd flask 
 
-# Create a user-defined bridge network with name my-net
+# Create a user-defined bridge network
 docker network create my-net
 
-# Create a volume for redis data
+# Create a volume for storing Redis data
 docker volume create redis-data
+
+# Create an image from local Dockerfile
+docker build -t flask:1.0 .
+
+# Create a flask container with name web 
+#    that attaches to network my-net
+#    and exports port 5000 to port 80
+docker create --name web    --network my-net \
+                            --publish 80:5000 flask:1.0
 
 # Create a redis container with name db 
 #    from image redis:5.0.5-alpine3.9 
 #    that mounts volume redis-data to container directory /data 
 #    and attaches to network my-net
 #    and executes redis-server on startup
-docker create --name db --network my-net -v redis-data:/data redis:5.0.5-alpine3.9 redis-server
+docker create --name db --network my-net \
+                        --volume redis-data:/data \
+                        redis:5.0.5-alpine3.9 redis-server
 
-# Start db container
-docker start db
+# Start containers
+docker start web db
 
-cd flask
-# Create custom image from local Dockerfile, named flask:latest
-docker build -t flask:latest .
-cd ..
 
-# Create a flask container with name web 
-#    from Dockerfile specified image
-#    that attaches to network my-net
-#    bind mounts the host directory app to /app inside the container
-#    and exports port 5000
-docker create --name web --network my-net -p 5000:5000 flask:latest
 
-# 7. Start web container
-docker start web
 
